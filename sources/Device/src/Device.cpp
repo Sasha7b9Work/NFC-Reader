a45c3130ecaf655e0a81024d64a7ef37911fc687
Sasha7b9Work <Phuntik-it@tut.by>
2022-06-09 12:45:50 +0300

    *

--- sources/Device/src/Device.cpp
+++ sources/Device/src/Device.cpp
@@ -4,44 +4,16 @@
 #include "Hardware/HAL/HAL.h"
 #include "Hardware/CDC/CDC.h"
 #include "Hardware/Timer.h"
-#include "Modules/ADC.h"
-#include "Modules/Barrier.h"
-#include "Processor.h"
 
 
 void Device::Init()
 {
     HAL::Init();
 
-    Barrier::Init();
-
-    Barrier::Open();
-
     Timer::Delay(500);
-
-    ADC::Init();
 }
 
 
 void Device::Update()
 {
-    if (Barrier::TimeElapsed() >= 200)
-    {
-//        Barrier::Switch();
-    }
-
-    if (ADC::DataReady())
-    {
-        float voltage = ADC::GetVoltage();
-
-        CDC::Transmit("%f", voltage);
-
-        Processor::AppendData(voltage, Barrier::IsOpened());
-
-        Processor::Log();
-    }
-    else
-    {
-        CDC::Transmit("Not data");
-    }
 }
