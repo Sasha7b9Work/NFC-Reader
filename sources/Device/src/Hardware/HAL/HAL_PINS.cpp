2847bcf62025499b056a72dd910c278e5e362f33
Sasha7b9 <Sasha7b9@tut.by>
2022-06-18 15:34:13 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL_PINS.cpp
+++ sources/Device/src/Hardware/HAL/HAL_PINS.cpp
@@ -16,9 +16,8 @@ void HAL_PINS::Init()
         GPIO_PIN_4  | GPIO_PIN_5  | GPIO_PIN_6  | GPIO_PIN_7  |
         GPIO_PIN_8  | GPIO_PIN_9  | GPIO_PIN_10 | GPIO_PIN_11 |
         GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
-        GPIO_MODE_OUTPUT_PP,
-        GPIO_PULLUP,
-        GPIO_SPEED_LOW
+        GPIO_MODE_ANALOG,
+        GPIO_NOPULL
     };
 
     HAL_GPIO_Init(GPIOA, &is);
@@ -39,6 +38,7 @@ void HAL_PINS::Init()
         GPIO_PIN_RESET);
 
     is.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+    is.Pin = GPIO_PIN_All;
 
     HAL_GPIO_Init(GPIOD, &is);
 
