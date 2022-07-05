911bc78e310cbfc33382592285c63b30aae87713
Sasha7b9 <Sasha7b9@tut.by>
2022-07-05 22:29:15 +0300

    *

--- sources/Sample/Drv/Spi/SpiLpcBus.h
+++ sources/Sample/Drv/Spi/SpiLpcBus.h
@@ -6,7 +6,13 @@ static const unsigned int LPC_SSP0_BASE = 1;
 static const int LPC_SSP0 = 1;
 
 
-class TSpiLpcBus
+struct LPC_SSP_TypeDef
 {
 
+};
+
+
+class TSpiLpcBus
+{
+    int swapBytes(LPC_SSP_TypeDef *, uint16_t, const uint8_t *, uint8_t *);
 };
