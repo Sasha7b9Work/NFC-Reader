4ac0fe0c551a5d159f94e910bb285f4eff5d1088
Sasha7b9 <Sasha7b9@tut.by>
2022-07-05 23:39:57 +0300

    *

--- sources/Sample/iso14443p3.h
+++ sources/Sample/iso14443p3.h
@@ -38,68 +38,68 @@
 #define Type_TagNPlay               0x7000
 
 
-struct TIso14443p3_Card: public TUid
+struct TIso14443p3_Card : public TUid
 {
- friend uint8_t *getDataAddrForOperation( uint8_t param, uint8_t *param_size );
- public:         
+    friend uint8_t *getDataAddrForOperation(uint8_t param, uint8_t *param_size);
+public:
 
-   // команды протокола ISO14443p3
-   static const uint8_t ISO14443_REQA_COMMAND             = 0x26;
-   static const uint8_t ISO14443_WUPA_COMMAND             = 0x52;
-   static const uint8_t ISO14443_ANTICOLLISION_1_COMMAND  = 0x93;
-   static const uint8_t ISO14443_ANTICOLLISION_2_COMMAND  = 0x95;
-   static const uint16_t ISO14443_HALT_COMMAND            = 0x5000;
-   
-   /*! Конструктор
-    * \param maf указатель на интерфейс с Analog frontend
-    */
-   TIso14443p3_Card( TCardAnalofInterface* maf )
-     {
-      this->Maf = maf;
-     }
-   
-   /*! Сброс карты питанием (выключением поля)
-    *  После применения поле остается включенным
-    */ 
-   void reset( ) const;
+    // команды протокола ISO14443p3
+    static const uint8_t ISO14443_REQA_COMMAND = 0x26;
+    static const uint8_t ISO14443_WUPA_COMMAND = 0x52;
+    static const uint8_t ISO14443_ANTICOLLISION_1_COMMAND = 0x93;
+    static const uint8_t ISO14443_ANTICOLLISION_2_COMMAND = 0x95;
+    static const uint16_t ISO14443_HALT_COMMAND = 0x5000;
 
-   /*! Переводит карту в состояние halt
-    */
-   void doHalt( ) const;
-   
-   /*!
-    * Выполянет комадну ATQA
-    * \return 0 если нет ответа и 1 если ответ есть и this->atq содержит ATQ
-    */
-   int doREQA( ) const;
-   
-   /*!
-    * Выполянет комадну WUPA
-    * \return 0 если нет ответа и 1 если ответ есть и this->atq содержит ATQ
-    */
-   int doWUPA( ) const;
-   
-   bool Activate( ) const;
-   int Anticollision( );
+    /*! Конструктор
+     * \param maf указатель на интерфейс с Analog frontend
+     */
+    TIso14443p3_Card(TCardAnalofInterface *maf)
+    {
+        this->Maf = maf;
+    }
 
-  int AuthenticateDES(  ) const;
- protected:
-   TCardAnalofInterface* Maf;      //!< указатель на интерфейс работы с картой
- 
-   mutable uint8_t atq[2];
-   uint8_t sak;
-   uint8_t type;
-   mutable int8_t  lastError;
-  
-   // блок данных для общения     
-   mutable uint8_t lastCommand;
-   mutable uint8_t answerLength;      
-   mutable uint8_t answer[36];
-  
-   int transceive( bool crc_enable, const uint8_t *tx, int tx_bit_num ) const;
-   int req( uint8_t cmd ) const;
-   int anticollisionStep( uint8_t cmd );
-   int SELECT( uint8_t level, const uint8_t uid[4] ) const;
+    /*! Сброс карты питанием (выключением поля)
+     *  После применения поле остается включенным
+     */
+    void reset() const;
+
+    /*! Переводит карту в состояние halt
+     */
+    void doHalt() const;
+
+    /*!
+     * Выполянет комадну ATQA
+     * \return 0 если нет ответа и 1 если ответ есть и this->atq содержит ATQ
+     */
+    int doREQA() const;
+
+    /*!
+     * Выполянет комадну WUPA
+     * \return 0 если нет ответа и 1 если ответ есть и this->atq содержит ATQ
+     */
+    int doWUPA() const;
+
+    bool Activate() const;
+    int Anticollision();
+
+    int AuthenticateDES() const;
+protected:
+    TCardAnalofInterface *Maf;      //!< указатель на интерфейс работы с картой
+
+    mutable uint8_t atq[2];
+    uint8_t sak;
+    uint8_t type;
+    mutable int8_t  lastError;
+
+    // блок данных для общения     
+    mutable uint8_t lastCommand;
+    mutable uint8_t answerLength;
+    mutable uint8_t answer[36];
+
+    int transceive(bool crc_enable, const uint8_t *tx, int tx_bit_num) const;
+    int req(uint8_t cmd) const;
+    int anticollisionStep(uint8_t cmd);
+    int SELECT(uint8_t level, const uint8_t uid[4]) const;
 };
 
 #endif
