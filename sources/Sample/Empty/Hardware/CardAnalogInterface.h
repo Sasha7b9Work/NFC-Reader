c3cb0b973e8ec3412dd992688393c656ce98e28d
Sasha7b9 <Sasha7b9@tut.by>
2022-07-06 09:43:26 +0300

    *

--- sources/Sample/Empty/Hardware/CardAnalogInterface.h
+++ sources/Sample/Empty/Hardware/CardAnalogInterface.h
@@ -6,5 +6,5 @@ class TCardAnalofInterface
 public:
     void fieldOff();
     void fieldOn();
-    int transceive(bool, const uint8_t *, int, uint8_t[36], size_t);
+    int transceive(bool crc_enable, const uint8_t *fifo, int num_bits_fifo, uint8_t *rx_buf, size_t size_buf);
 };
